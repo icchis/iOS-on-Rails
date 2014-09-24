63c055ad51289da3125f7db0012415c116ce4fa0
s.111chikiii <s.111chikiii@gmail.com>
2014-09-24 20:13:22 +0900

    add inter serve

--- UW_Rails/app/controllers/public_controller.rb
+++ UW_Rails/app/controllers/public_controller.rb
@@ -1,5 +1,7 @@
 class PublicController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
   end
 end
--- UW_Rails/config/environments/development.rb
+++ UW_Rails/config/environments/development.rb
@@ -4,6 +4,7 @@ UWRails::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -31,6 +32,8 @@ UWRails::Application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  config.middleware.delete Rack::Lock
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
